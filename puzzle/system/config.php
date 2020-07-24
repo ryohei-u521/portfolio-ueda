afc8f2f2f0e40e1b7a719d4d84cbe2d2da2fe0a0
ryohei-u521 <webdesi521@gmail.com>
2020-07-24 18:28:22 +0900

    二回目のコミット

--- puzzle/system/config.php
+++ puzzle/system/config.php
@@ -3,4 +3,4 @@
 $db_host = "mysql1.php.starfree.ne.jp"; // データベースのホスト
 $db_name = "puzzlesubsc_ryohei0521"; // データベースの名前
 $db_user = "puzzlesubsc_0521"; // データベース接続ユーザー
-$db_pass = "ryohei0521"; // データベース接続パスワード
+$db_pass = ""; // データベース接続パスワード
